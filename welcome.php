2971c8c3e33a0853a4f2854738d5e4f8d5b6d3f3
WilkiSwin406 <105927777@student.swin.edu.au>
2025-04-30 12:08:58 +1000

    finished

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+if (isset($_SESSION['user'])) {
+    echo "Welcome, " . htmlspecialchars($_SESSION['user']) . "!";
+} else {
+    header('Location: login.php');
+    exit();
+}
+?>
